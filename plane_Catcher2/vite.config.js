7cd228c31eba4c0c8870720c8d86382dd274fdd2
Emire HAOUAS <97857043+EmireHaouas@users.noreply.github.com>
2024-10-26 04:23:39 +0100

    Update vite.config.js
    Change of vite.js for GitHub

--- plane_Catcher2/vite.config.js
+++ plane_Catcher2/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/nom-du-depot/' // Remplacez 'nom-du-depot' par le nom exact de votre dépôt GitHub
 })
