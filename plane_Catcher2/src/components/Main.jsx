1cc413b5a8841efc83922e0d422ba6ef0cd100ab
EmireHaouas <emire.haouas100@gmail.com>
2025-03-12 17:05:07 +0100

    commit before new branch

--- plane_Catcher2/src/components/Main.jsx
+++ plane_Catcher2/src/components/Main.jsx
@@ -1,15 +1,8 @@
 import React, { useState } from 'react';
 import '../Main.css';
+import FTracker from './FTracker/FTracker';
 import image_header from '../assets/imgs/img_header.png';
-import Arrow_img from '../assets/imgs/arrow.png';
 import active_Flight from '../assets/imgs/active_icon.gif';
-import clock_icon from '../assets/imgs/clock_icon.png';
-import plane_moving from '../assets/imgs/plane_moving.gif';
-import Bottom_arrow_icon from '../assets/imgs/Bottom_arrow_icon.png';
-import takeoff_plane from '../assets/imgs/takeoff_plane.gif';
-import landing_plane from '../assets/imgs/landing_plane.gif';
-import tower from '../assets/imgs/tower.gif';
-import flying_plane from '../assets/imgs/flying_plane.gif';
 import step_img from '../assets/imgs/step_img.jpg';
 import spec1 from '../assets/imgs/spec1.jpg';
 import window_sea from '../assets/imgs/window_sea.jpg';
@@ -24,7 +17,7 @@ const Main = () => {
     const [date, setDate] = useState('');
     const [flightData, setFlightData] = useState(null);
     const [error, setError] = useState(null);
-    const [isGeekInfosVisible, setIsGeekInfosVisible] = useState(false); // Ajout de l'état pour la visibilité des infos geek
+    const [isGeekInfosVisible, setIsGeekInfosVisible] = useState(false); 
 
     const formatLocalTime = (timestamp) => {
         const date = new Date(timestamp);
@@ -71,7 +64,7 @@ const Main = () => {
     };
 
     const toggleGeekInfosVisibility = () => {
-        setIsGeekInfosVisible(!isGeekInfosVisible); // Changer la visibilité
+        setIsGeekInfosVisible(!isGeekInfosVisible); 
     };
 
     return (
@@ -88,122 +81,12 @@ const Main = () => {
                         unforgettable experience.
                     </p>
                     <button className='button1'>Discover Now</button>
+                    {console.log('Hello World')}
                 </div>
             </article>
+            <FTracker />
 
-            <section id='form_Iata' className='Form_iata'>
-                <h1 className='h1_form'>Track Your Flight Now</h1>
-
-                {/*/test image landing et t/o */}
-                <div className="tolanding_icons">
-                <img alt='' className='landing_plane' src={landing_plane}/>
-                <img alt='' className='tower_icon' src={tower}/>
-                <img alt='' className='takeoff_plane' src={takeoff_plane}/>
-               </div>
-                <form className='form_track' onSubmit={handleSubmit}>
-                    <div className='form_group'>
-                        <input
-                            className='flight_idnumber'
-                            type="text"
-                            id="flightId"
-                            value={flightId}
-                            onChange={(e) => setFlightId(e.target.value)}
-                            placeholder=' '
-                            required
-                        />
-                        <label htmlFor="flightId">Flight Number</label>
-                    </div>
-
-                    <input
-                        className='date_Flight'
-                        type="date"
-                        id="date"
-                        value={date}
-                        onChange={(e) => setDate(e.target.value)}
-                        required
-                    />
-
-                    <button className='button_submit' type="submit">Track My Flight</button>
-                </form>
-
-                {error && <p className="error">{error}</p>}
-                {flightData && (
-                    <div className='card_Flightinfos'>
-                        <div className='card_details'>
-                            <div className="h3_moving">
-                            <h3 className='h3_cards'>Flight Details</h3> 
-                            <img className='flying_plane' src={flying_plane} alt=''/>
-                            </div>
-                          <div className="entiresection_wrap">
-                            <div className="entire_section">
-                                <div className="zero_Row">
-                                <p>{flightData.airline.name} {flightData.flight.number}</p>
-                                <p>{flightData.flight.iata}</p>
-
-                                </div>
-                                
-                                <div className="first_Row">
-                                    <p className='airport_Dep'>{flightData.departure.airport}</p>
-                                    <p>
-                                        <span className='gates_design'>
-                                            <img className='Arrow_img' alt='arrow' src={Arrow_img} />
-                                            {flightData.departure.gate}
-                                        </span>
-                                    </p>
-                                </div>
-
-                                <div className="second_Row">
-                                    <p className='departure_Time'>{formatLocalTime(flightData.departure.estimated)}</p>
-                                    <p>
-                                        <span className='terminals_design'>
-                                            <img className='Arrow_img' alt='arrow' src={Arrow_img} />
-                                            {flightData.departure.terminal}
-                                        </span>
-                                    </p>
-                                </div>
-
-                                <div className="third_Row">
-                                    <img className='clock_icon' alt='clock icon' src={clock_icon} />
-                                    <span>{calculateFlightDuration(flightData.departure.estimated, flightData.arrival.estimated)}</span> <hr />
-                                </div>
-
-                                <div className="fourth_Row">
-                                    <p className='airport_arrival'>{flightData.arrival.airport}</p>
-                                    <p>
-                                        <span className='gates_design'>
-                                            <img className='Arrow_img' alt='arrow icon' src={Arrow_img} />
-                                            {flightData.arrival.gate}
-                                        </span>
-                                    </p>
-                                </div>
-
-                                <div className="sixth_Row">
-                                    <p className='arrival_Time'>{formatLocalTime(flightData.arrival.estimated)}</p>
-                                    <p>
-                                        <span className='terminals_design'>
-                                            <img className='Arrow_img' alt='arrow icon' src={Arrow_img} />
-                                            {flightData.arrival.terminal}
-                                        </span>
-                                    </p>
-                                </div>
-                            </div>
-                            </div>
-
-                            <p>I am an aviation geek.</p>
-                            <ToggleButton onClick={toggleGeekInfosVisibility} />
-                            {isGeekInfosVisible && (
-                                <div className='geek_infos'>
-                                <p className='airline'><span>Airline:</span> {flightData.airline.name}</p>
-                                <p><span>Status:</span> {flightData.flight_status}</p>
-                                <p><span>Aircraft Model:</span> {flightData.aircraft.icao}</p>
-                                <p><span>Departure Delay:</span> {flightData.departure.delay}</p>
-                                <p><span>Tail Number:</span> <span>{flightData.aircraft.registration}</span></p>
-                            </div>
-                            )}
-                        </div>
-                    </div>
-                )}
-            </section>
+           
             <section id='track_Steps' className='track_Steps'>
                 <div className=''>
                     <h2 className='h2_Steps'>Track Your next Flight<br></br>
@@ -284,16 +167,12 @@ const Main = () => {
                         <p className='p_Box3'>15+</p>
                         <p className='p2_Box3'>Years Experience</p>
                     </div>
-                  
                  </div>
                 </div>
                 </div>
             </section>
         </main>
-        
     );
 };
 
-
-{/*Developed by Emire H */}
 export default Main;
